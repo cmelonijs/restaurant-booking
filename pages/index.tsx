2b7d9ff79ef24e09e047c40d8598c9eef5f06ab8
cmelonijs <carlomeloni63@gmail.com>
2023-03-26 15:04:30 +0200

    feat: adding html templates

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,5 @@ import styles from "../styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main></main>
-  );
+  return <main></main>;
 }

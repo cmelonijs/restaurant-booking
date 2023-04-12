bdc65280d56f228dec60e6f39fc9659dc160d38a
cmelonijs <carlo.meloni@abinsula.com>
2023-04-12 09:37:53 +0200

    feat: fetch single restaurant by slug

--- pages/restaurant/[slug]/index.tsx
+++ pages/restaurant/[slug]/index.tsx
@@ -7,8 +7,24 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-const RestaurantDetailsPage = () => {
+const prisma = new PrismaClient();
+
+type Props = {
+    restaurant: {
+    created_at: string | undefined;
+    updated_at: string | undefined;
+    slug?: string | undefined;
+    id?: number | undefined;
+    name?: string | undefined;
+    images?: string[] | undefined;
+    description?: string | undefined;
+  }
+}
+
+const RestaurantDetailsPage = ({restaurant}: Props) => {
+  console.log('restaurant inside component', restaurant)
   return (
     <>
       <Header />
@@ -28,5 +44,32 @@ const RestaurantDetailsPage = () => {
     </>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const slug = context.params.slug;
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+      created_at: true,
+      updated_at: true,
+    }
+  });
+
+  const serializedRestaurants = {
+      ...restaurant,
+      created_at: restaurant?.created_at.toISOString(),
+      updated_at: restaurant?.updated_at.toISOString(),
+    };
+
+  return { props: { restaurant: serializedRestaurants } };
+}
 
 export default RestaurantDetailsPage;

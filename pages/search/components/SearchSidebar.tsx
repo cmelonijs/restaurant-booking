67ef90530348dd842ac1dd58ef273286149c2d58
cmelonijs <carlo.meloni@abinsula.com>
2023-04-14 11:22:20 +0200

    feat: building a query for searching restaurtants

--- pages/search/components/SearchSidebar.tsx
+++ pages/search/components/SearchSidebar.tsx
@@ -1,4 +1,6 @@
 import { Location, Cuisine } from "@prisma/client";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 interface Props {
   locations: Location[],
@@ -6,38 +8,49 @@ interface Props {
 }
 
 const SearchSidebar = ({locations, cuisines}: Props) => {
-  console.log('locations', locations)
-  console.log('cuisines', cuisines)
+  const router = useRouter()
+  const [queries, setQueries] = useState({
+    location: '',
+    cuisine: '',
+    price: ''
+  })
+  const {location, cuisine, price} = queries;
+
+  const buildQuery = (location: string, cuisine: string, price: string) => {
+    return `/search${location ? `?city=${location}` : `?city=ottawa`}${cuisine && `&cuisine=${cuisine}`}${price && `&price=${price}`}`
+  };
+
   return (
     <div className="w-1/5">
       <div className="border-b pb-4">
         <h1 className="mb-2">Region</h1>
         {locations.map((loc: Location) => (
-          <p key={loc.id} className="font-light text-reg">{loc.name}</p>
+          <p onClick={() => setQueries({...queries, location: loc.name})} key={loc.id} className="font-light text-reg">{loc.name}</p>
         ))}
       </div>
       <div className="border-b pb-4 mt-3">
         <h1 className="mb-2">Cuisine</h1>
         {cuisines.map((cuisine: Cuisine) => (
-          <p key={cuisine.id} className="font-light text-reg">{cuisine.name}</p>
+          <p onClick={() => setQueries({...queries, cuisine: cuisine.name})} key={cuisine.id} className="font-light text-reg">{cuisine.name}</p>
         ))}
       </div>
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
         <div className="flex">
-          <button className="border w-full text-reg font-light rounded-l p-2">
+          <button onClick={() => setQueries({...queries, price: 'CHEAP'})} className="border w-full text-reg font-light rounded-l p-2">
             $
           </button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2">
+          <button onClick={() => setQueries({...queries, price: 'REGULAR'})} className="border-r border-t border-b w-full text-reg font-light p-2">
             $$
           </button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
+          <button onClick={() => setQueries({...queries, price: 'EXPENSIVE'})} className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
             $$$
           </button>
         </div>
       </div>
-    </div>
-  );
-};
+      {(location || cuisine || price) && <button onClick={() => router.push(`${buildQuery(location, cuisine, price)}`)} style={{border: '1px solid red'}}>cerca</button>}
+      </div>
+)}
 
-export default SearchSidebar;
+export default SearchSidebar
+      
